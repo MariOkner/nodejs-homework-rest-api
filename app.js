74c4b159772b0c5e25805a44d7bdfc4e5c9204ab
Marina <mariokner@gmail.com>
2023-01-10 15:04:45 +0200

    REST API

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// middlewares
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+// routes
+app.use("/api/contacts", contactsRouter);
 
+// 404
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  if (err.status) {
+    res.status(err.status).json({
+      message: err.message,
+    });
+  }
 
-module.exports = app
+  res.status(500).json({
+    message: "Internal server error",
+  });
+});
+
+// app.listen(3000, () => {
+//   console.log("server is listening on port 3000");
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, function () {
+//   console.log(`Server running. Use our API on port: ${PORT}`);
+// });
+
+module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,64 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts");
+const { HttpError } = require("../../helpers");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contactsList = await contacts.listContacts();
+  console.log(contactsList);
+  res.json(contactsList);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await contacts.getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (!contact) {
+    // return next(HttpError(404, "Not found!!!!!"));
+    return res.status(404).json({ message: "Not" });
+  }
+  res.json(contact);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (!name || !email || !phone) {
+    return next(HttpError(400, "Missing required name field!"));
+    // return res.status(400).json({ message: "missing required name field" });
+  }
+  const newContact = await contacts.addContact(name, email, phone);
 
-module.exports = router
+  res.status(201).json(newContact);
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await contacts.getContactById(contactId);
+
+  if (!contact) {
+    next(HttpError(404, "Not found"));
+  }
+  await contacts.removeContact(contactId);
+  res.status(200).json({ message: "contact deleted" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
+
+  if (!name && !email && !phone) {
+    return next(HttpError(400, "Missing fields"));
+  }
+
+  const updatedContact = await contacts.updateContact(contactId, name, email, phone);
+
+  if (!updatedContact) {
+    res.status(404).json({ message: "Not found" });
+    return;
+  }
+  res.status(200).json(updatedContact);
+});
+
+module.exports = router;
