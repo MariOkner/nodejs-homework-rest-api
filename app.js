ac850a85ea3b8b7dffc497ac81a57f1befa6a473
Marina <mariokner@gmail.com>
2023-01-19 23:58:42 +0200

    cleaned up the code

--- app.js
+++ app.js
@@ -27,24 +27,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-// app.use((error, req, res, next) => {
-//   if (error.status) {
-//     return res.status(error.status).json({
-//       message: error.message,
-//     });
-//   }
-
-//   if (error.message.includes("Cast to ObjectId failed for value")) {
-//     return res.status(400).json({
-//       message: "id is invalid",
-//     });
-//   }
-
-//   return res.status(500).json({
-//     message: "Internal server error",
-//   });
-// });
-
 module.exports = {
   app,
 };
